e5bce3f4d0fa3420ac532ec8a964de36f67ec577
r.zubehin <github1991zubehin2605>
2020-10-02 15:05:53 +0300

    method readFromExel return int

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,40 +7,44 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.*;
 
 public class Test {
-    private final static File fileRead = new File("C:\\Users\\user\\Desktop\\план.xlsx");
-    private final static File fileWrite = new File("C:\\Users\\user\\Desktop\\нрм.xlsx");
+    private final static File fileRead = new File("C:\\план.xlsx");
+    private final static File fileWrite = new File("C:\\нрм тест.xlsx");
+    private int firstCellRead = 2;
     public static void main(String[] args) {
-        readFromExel(fileRead, 2, 3, 6);
-        writeIntoExel(fileWrite);
+        int q = readFromExel(fileRead, 2, 3, 6);
+        writeIntoExel(fileWrite, q);
     }
 
-    public static void readFromExel(File file, int numArticle, int numName, int numQuantity) {
+    public static int readFromExel(File file, int numArticle, int numName, int numQuantity) {
+        int q = 0;
+        XSSFWorkbook myExelBook = null;
         try {
-            XSSFWorkbook myExelBook = new XSSFWorkbook(new FileInputStream(file));
-            XSSFSheet myExelSheet = myExelBook.getSheet("Лист1");
-            XSSFRow row = myExelSheet.getRow(1);
-
-            if (row.getCell(numArticle).getCellType() == CellType.STRING) {
-                System.out.println(row.getCell(numArticle).getStringCellValue());
-            }
-            if (row.getCell(numName).getCellType() == CellType.STRING){
-                System.out.println(row.getCell(numName).getStringCellValue());
-            }
-            if (row.getCell(numQuantity).getCellType() == CellType.NUMERIC) {
-                System.out.println(row.getCell(numQuantity).getNumericCellValue());
-            }
+            myExelBook = new XSSFWorkbook(new FileInputStream(file));
             myExelBook.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        XSSFSheet myExelSheet = myExelBook.getSheet("Лист1");
+        XSSFRow row = myExelSheet.getRow(1);
+
+        if (row.getCell(numArticle).getCellType() == CellType.STRING) {
+            System.out.println(row.getCell(numArticle).getStringCellValue());
+        }
+        if (row.getCell(numName).getCellType() == CellType.STRING){
+            System.out.println(row.getCell(numName).getStringCellValue());
+        }
+        if (row.getCell(numQuantity).getCellType() == CellType.NUMERIC) {
+            q = (int) row.getCell(numQuantity).getNumericCellValue();
+        }
+        return q;
     }
-    public static void writeIntoExel(File fileWrite){
+    public static void writeIntoExel(File fileWrite, int quantity){
         try {
             FileInputStream fileInputStream = new FileInputStream(fileWrite);
             XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
             XSSFSheet sheet = xssfWorkbook.getSheetAt(0);
             XSSFCell cell = sheet.getRow(1).getCell(3);
-            cell.setCellValue("add value");
+            cell.setCellValue(quantity);
             fileInputStream.close();
 
             FileOutputStream fileOutputStream = new FileOutputStream(fileWrite);
