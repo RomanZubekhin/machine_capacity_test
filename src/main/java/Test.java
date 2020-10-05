1df5eb6bf911e694725c7d89520602b5d63761fc
Roman <r.zubehin@gmail.com>
2020-10-06 00:21:14 +0300

    some fix

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,19 +3,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.Map;
+//import java.util.Map;
 
 public class Test {
     private final static File fileRead = new File("C:\\Users\\user\\Desktop\\план.xlsx");
     //private final static File fileWrite = new File("C:\\Users\\user\\Desktop\\нрм.xlsx");
+    private final static HashMap<String,Integer> hashMap = new HashMap<String, Integer>();
 
     public static void main(String[] args) throws IOException {
         readFromExel(fileRead, 2, 6);
         //print(hashMap);
         //writeIntoExel(fileWrite, 2);
-
     }
-    private final static HashMap<String,Integer> hashMap = new HashMap<String, Integer>();
 
     public static void readFromExel(File file, int numArticle, int numQuantity) throws IOException {
         int count = 1;
@@ -41,7 +40,32 @@ public class Test {
         }
     }
 
-/*    public static void print(HashMap<String,Integer> map){
+    public static void writeIntoExel(File fileWrite, HashMap<String, Integer> map){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileWrite);
+            Workbook wb = new XSSFWorkbook(fileInputStream);
+            Sheet sheet = wb.getSheetAt(0);
+            int startCellColumn = 2;
+            int startCellRow = 1;
+            Cell cell= sheet.getRow(1).getCell(3);
+            cell.setCellValue(3);
+
+            //Re-evaluate formulas with POI's FormulaEvaluator
+            wb.getCreationHelper().createFormulaEvaluator().evaluateAll();
+            fileInputStream.close();
+            //write data
+            FileOutputStream fileOutputStream = new FileOutputStream(fileWrite);
+            wb.write(fileOutputStream);
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+/*
+    public static void print(HashMap<String,Integer> map){
         String article;
         int quantity;
         int count = 1;
@@ -50,28 +74,6 @@ public class Test {
             quantity = pair.getValue();
             System.out.println(count + ") Article = " + article + " " + "Quantity = " + quantity);
             count++;
-        }
-    }*/
-/*
-    public static void writeIntoExel(File fileWrite, int quantity){
-        try {
-            FileInputStream fileInputStream = new FileInputStream(fileWrite);
-            Workbook wb = new XSSFWorkbook(fileInputStream);
-            Sheet sheet = wb.getSheetAt(0);
-            Cell cellQuantity = sheet.getRow(1).getCell(3);
-            cellQuantity.setCellValue(quantity);
-
-            //Re-evaluate formulas with POI's FormulaEvaluator
-            wb.getCreationHelper().createFormulaEvaluator().evaluateAll();
-
-            fileInputStream.close();
-            FileOutputStream fileOutputStream = new FileOutputStream(fileWrite);
-            wb.write(fileOutputStream);
-            fileOutputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 */
